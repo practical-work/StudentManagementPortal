ee8f3ddeeee3c135af823ea2a7ff4906ccc8599b
practical-work <62kunalvats@gmail.com>
2026-07-31 22:15:03 +0530

    update config file

--- app/config.py
+++ app/config.py
@@ -1,21 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# class Config:
-#     SECRET_KEY = os.getenv("SECRET_KEY")
-#     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
-#     MAIL_SERVER = "smtp.gmail.com"
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-#     MAIL_DEFAULT_SENDER = ("Student Management Portal",os.getenv("MAIL_USERNAME"))
-
-
 import os
 from dotenv import load_dotenv
 
@@ -23,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ["MYSQL_URL"]
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
     MAIL_SERVER = "smtp.gmail.com"
@@ -31,4 +13,6 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = ("Student Management Portal", os.getenv("MAIL_USERNAME"))
+    MAIL_DEFAULT_SENDER = ("Student Management Portal",os.getenv("MAIL_USERNAME"))
+
+
