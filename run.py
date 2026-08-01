a3421b53208a55db263e0e70c6b042eceb38f843
practical-work <62kunalvats@gmail.com>
2026-08-01 11:05:35 +0530

    updated files

--- run.py
+++ run.py
@@ -21,96 +21,9 @@ from app.models.student_personal import StudentPersonal
 
 app = create_app()
 
-def seed(model, field_name, values):
-
-    if model.query.count() == 0:
-
-        for value in values:
-            obj = model()
-
-            setattr(obj, field_name, value)
-
-            student_db.session.add(obj)
-
-        student_db.session.commit()
-
-        print(f"{model.__tablename__} seeded.")
-
-    else:
-        print(f"{model.__tablename__} already contains data.")
-
-
-
 with app.app_context():
     student_db.create_all()
-    print("Tables Created Successfully.")
-
-    inspector = inspect(student_db.engine)
-    print(inspector.get_table_names())
-
-    seed(
-            MasterGender,
-            "gender_name",
-            [
-                "Male",
-                "Female",
-                "Other"
-            ]
-        )
     
-    seed(
-            MasterCategory,
-            "category_name",
-            [
-                "General",
-                "EWS",
-                "OBC",
-                "SC",
-                "ST"
-            ]
-        )
-    
-    seed(
-            MasterReligion,
-            "religion_name",
-            [
-                "Hindu",
-                "Muslim",
-                "Sikh",
-                "Christian",
-                "Jain",
-                "Buddhist",
-                "Parsi",
-                "Other"
-            ]
-        )
-    
-    seed(
-            MasterNationality,
-            "nationality_name",
-            [
-                "Indian"
-            ]
-        )
-    
-    seed(
-            MasterBloodGroup,
-            "blood_group_name",
-            [
-                "A+",
-                "A-",
-                "B+",
-                "B-",
-                "AB+",
-                "AB-",
-                "O+",
-                "O-"
-            ]
-        )
-    
-    print("Master data seeded successfully.")
-
-
 
 if __name__ == "__main__":
     app.run(debug=False)
